1d41647e6da16bb3e02639eb92d724d1678527de
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2023-11-09 15:52:56 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './Components/navbar/Navbar';
 import Home from './Components/Homepage/Home';
 import Media from './Components/Media/Media';
 import ScrollToTop from './Components/scrollToTop/scrollToTop'
-// import MemberNav from './Components/Members Page/MemberNavbar/MemberNav.jsx'
+
 function App() {
   return (
     <>
